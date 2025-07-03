53aa94de0d5b18ec26bc71225b583274da719a28
Voxeon7 <trabalho.tashiro@gmail.com>
2025-07-03 15:20:52 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -761,7 +761,7 @@ function App() {
                     <div className="absolute inset-0 bg-gradient-to-r from-yellow-300/20 to-amber-400/20 animate-pulse"></div>
                     <span className="relative flex items-center justify-center space-x-2">
                       <Check className="w-5 h-5" />
-                      <span>¡OBTÉN TU TRANSFORMACIÓN!</span>https://pay.kiwify.com/zdRY0u2
+                      <span>¡OBTÉN TU TRANSFORMACIÓN!</span>
                       <ArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform" />
                     </span>
                   </button>
@@ -867,7 +867,7 @@ function App() {
                       <div className="absolute inset-0 bg-gradient-to-r from-yellow-300/20 to-amber-400/20 animate-pulse"></div>
                       <span className="relative flex items-center justify-center space-x-3">
                         <Check className="w-6 h-6" />
-                        <span>¡OBTÉN TU TRANSFORMACIÓN!</span>https://pay.kiwify.com/zdRY0u2
+                        <span>¡OBTÉN TU TRANSFORMACIÓN!</span>
                         <ArrowRight className="w-6 h-6 group-hover:translate-x-1 transition-transform" />
                       </span>
                     </button>
